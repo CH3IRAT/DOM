e902c7e42f96d52a3c9624bc2ec7de4b776fe763
CH3IRAT <83812428+CH3IRAT@users.noreply.github.com>
2021-06-01 10:42:42 +0200

    Add files via upload

---
+++ test.js
@@ -0,0 +1,44 @@
+var btn1= document.getElementById("plus1")
+ var count1= document.getElementById("counter1") 
+  btn1.addEventListener('click',function()
+  {     count1.value++ })
+   
+  var btn1= document.getElementById("moin1")
+ var count1= document.getElementById("counter1") 
+  btn1.addEventListener('click',function()
+  {     count1.value-- })
+   
+
+  var btn2= document.getElementById("plus2")
+  var count2= document.getElementById("counter2") 
+   btn2.addEventListener('click',function()
+   {     count2.value++ })
+    
+   var btn2= document.getElementById("moin2")
+  var count2= document.getElementById("counter2") 
+   btn2.addEventListener('click',function()
+   {     count2.value-- })
+    
+
+
+
+   
+  var btn3= document.getElementById("plus3")
+  var count3= document.getElementById("counter3") 
+   btn3.addEventListener('click',function()
+   {     count3.value++ })
+    
+   var btn3= document.getElementById("moin3")
+  var count3= document.getElementById("counter3") 
+   btn3.addEventListener('click',function()
+   {     count3.value-- })
+
+    
+
+     
+  
+ 
+
+
+  
+
